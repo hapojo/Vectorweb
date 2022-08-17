e73746a0cdb84b071d42225411f868d869f4d1c7
Happiness ojo <48913853+hapojo@users.noreply.github.com>
2022-08-17 17:27:34 +0100

    TO BE HOSTED

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-
+console.log("hereeee");
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('/*', function (req, res) {
